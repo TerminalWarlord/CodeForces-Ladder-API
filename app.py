e0d3f89de888cecfa60cdf959b71059a00c74e31
TerminalWarlord <joybiswas389@gmail.com>
2022-03-26 17:42:53 +0600

    cors added

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ from main import main
 import uvicorn
 
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/{rating}/{handle}")
 async def read_item(rating, handle):
